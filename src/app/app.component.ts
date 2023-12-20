55bed125b2eb6cd84622123cc921a8cc54365950
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2023-12-20 06:00:14 +0530

    Basic Drag Drop Using Cdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NgCdkDndComponent } from './ng-cdk-dnd/ng-cdk-dnd.component';
 import { SimpleComponent } from './simple/simple.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SimpleComponent],
+  imports: [SimpleComponent, NgCdkDndComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/ng-cdk-dnd/ng-cdk-dnd.component.ts
@@ -0,0 +1,44 @@
+import {
+  CdkDragDrop,
+  CdkDragExit,
+  DragDropModule,
+  copyArrayItem,
+} from '@angular/cdk/drag-drop';
+import { NgFor, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-cdk-dnd',
+  standalone: true,
+  imports: [DragDropModule, NgFor, NgIf],
+  templateUrl: './ng-cdk-dnd.component.html',
+  styleUrl: './ng-cdk-dnd.component.scss',
+})
+export class NgCdkDndComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  transferringItem: string | undefined | string[] = undefined;
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer.id !== event.container.id) {
+      copyArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+
+    this.transferringItem = undefined;
+  }
+
+  entered(event: any) {
+    this.transferringItem = undefined;
+  }
+
+  exited(e: CdkDragExit<string[]>) {
+    this.transferringItem = e.item.data;
+  }
+}
